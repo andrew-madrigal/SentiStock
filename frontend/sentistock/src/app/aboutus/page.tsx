8d1e7096dedf02cd4864c8638a368bc159ee5800
Anupriya <anupriyaislam@gmail.com>
2025-03-08 20:56:56 -0800

    Added basic page and component files to directory

---
+++ frontend/sentistock/src/app/aboutus/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">About Us/Project Description</h1>
+      </div>
+    );
+  }
+  
---
+++ frontend/sentistock/src/app/stockanalysis/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">Stock Analysis</h1>
+      </div>
+    );
+  }
+  
