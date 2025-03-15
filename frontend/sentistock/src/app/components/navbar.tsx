7afb7b06ae01c19d2407f839e81bd7dd383ab4a6
Anupriya <anupriyaislam@gmail.com>
2025-03-15 11:44:58 -0700

    Added about us page and basic navigation

--- frontend/sentistock/src/app/components/navbar.tsx
+++ frontend/sentistock/src/app/components/navbar.tsx
@@ -1,2 +1,22 @@
-//Navbar Componenet
-  
+"use client";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="font-inconsolata text-xl font-bold hover:text-green-300">
+          SentiStock
+        </Link>
+
+        <div className="font-inconsolata flex space-x-6">
+          <Link href="/aboutus" className="hover:text-green-300">About Us</Link>
+          <Link href="/stockanalysis" className="hover:text-green-300">Stock Analysis</Link>
+          <Link href="/trendingstocks" className="hover:text-green-300">Trending Stocks</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
