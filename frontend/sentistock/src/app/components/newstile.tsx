d40b66c9ca85d02f830bc894f09e7e0958bcd25d
9158pebble <finnazxw@gmail.com>
2025-04-06 23:39:26 -0700

    added tiles to trending stocks page

--- frontend/sentistock/src/app/components/newstile.tsx
+++ frontend/sentistock/src/app/components/newstile.tsx
@@ -1,3 +1,4 @@
+
 //Card/ Stock News Tile Component
 //interface setup
 interface NewsTileProps {
@@ -6,25 +7,28 @@ interface NewsTileProps {
   img_src_link: string;
 };
 
-//component setup
+"use client";
+
 const NewsTile: React.FC<NewsTileProps> = ({title, article_link, img_src_link}) => {
+  //component setup
   function directClick() {
     window.open(article_link, "_blank"); // use this instead to open in new tab (better UX)
   };
   return (
     //feel free to play around with the code in className, this is tailwindCSS and is used to design the button!
     <button onClick={directClick} 
-    className="flex-1 items-center space-x-2 p-2 bg-slate-100 shadow-md rounded-lg border-2-transparent 
+    className="flex-2 items-center space-x-2 p-2 bg-slate-100 shadow-md rounded-lg border-2-transparent 
     hover:bg-slate-200 hover:border-[#adc178] transition">
       <img
         src= {img_src_link}
         alt="Stock news button"
-        width={20}
-        height={20}
-        className = "opacity-50 hover:opacity-25 hover:backdrop-blur-xs-grayscale"
+        width={393}
+        height={300}
+        className = "opacity-50 items-center hover:opacity-25 hover:backdrop-blur-xs-grayscale"
       />
-      <span className="text-left-gray-900 font-medium">{title}</span>
+      <span className="text-center-gray-900 font-medium flex-2">{title}</span>
     </button>
   );
 };
 export default NewsTile;
+
